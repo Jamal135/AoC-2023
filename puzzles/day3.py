a0af9fbfb2e43426ae7534c7aeff23826767692b
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-12-03 16:30:41 +1000

    Attempt on day 3 part 1

--- puzzles/day3.py
+++ puzzles/day3.py
@@ -1,14 +1,27 @@
 ''' Created: '''
 
-day = 0
+day = 3
 
 # External imports
+import re
 
 # Internal imports
 from aoc import AOC
 
 def part_1(puzzle: str):
-    pass
+    line_length = len(puzzle.split('\n')[0])
+    symbols = set(char for char in puzzle if not char.isdigit() and char != '.')
+    puzzle = puzzle.replace('\n', '')
+    total = 0
+    for index, character in enumerate(puzzle):
+        if character in symbols:
+            x, y = divmod(index, line_length)
+            base_index = x * line_length + y
+            get_slice = lambda offset: puzzle[(base_index + offset - 3):(base_index + offset + 4)]
+            data = " ".join(["", get_slice(-line_length), get_slice(0), get_slice(line_length), ""])
+            matches = re.findall(r'\d{3}|(?<=\.)\d{2}(?=\.)|(?<=\.\S)\d(?=\S\.)', data)
+            total += sum(map(int, matches))
+    return total
 
 def part_2(puzzle: str):
     pass
