5a937e5b496189ea6b3d715e9b6934d3880d8098
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-12-03 20:55:17 +1000

    Completed day 3

--- puzzles/day3.py
+++ puzzles/day3.py
@@ -18,17 +18,30 @@ def part_1(puzzle: str):
             x, y = divmod(index, line_length)
             base_index = x * line_length + y
             get_slice = lambda offset: puzzle[(base_index + offset - 3):(base_index + offset + 4)]
-            data = " ".join(["", get_slice(-line_length), get_slice(0), get_slice(line_length), ""])
-            matches = re.findall(r'\d{3}|(?<=\.)\d{2}(?=\.)|(?<=\.\S)\d(?=\S\.)', data)
+            data = ' '.join(['' + get_slice(-line_length), get_slice(0), get_slice(line_length) + ''])
+            matches = re.findall(r'\d{3}|(?<=\S)\d{2}(?=\S)|(?<=\S{2})\d{1}(?=\S{2})', data)
             total += sum(map(int, matches))
     return total
 
 def part_2(puzzle: str):
-    pass
+    line_length = len(puzzle.split('\n')[0])
+    puzzle = puzzle.replace('\n', '')
+    total = 0
+    for index, character in enumerate(puzzle):
+        if character == '*':
+            x, y = divmod(index, line_length)
+            base_index = x * line_length + y
+            get_slice = lambda offset: puzzle[(base_index + offset - 3):(base_index + offset + 4)]
+            data = ' '.join(['' + get_slice(-line_length), get_slice(0), get_slice(line_length) + ''])
+            matches = re.findall(r'\d{3}|(?<=\S)\d{2}(?=\S)|(?<=\S{2})\d{1}(?=\S{2})', data)
+            if len(matches) == 2:
+                gear_ratio = int(matches[0]) * int(matches[1])
+                total += gear_ratio
+    return total
 
 if __name__ == '__main__':
     aoc = AOC(day)
     puzzle = aoc.get_puzzle()
-
+    
     print(part_1(puzzle))
     print(part_2(puzzle))
