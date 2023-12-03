71cdef947b826edbf88f9fe4c30ed77d836b5a1d
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-12-03 22:00:49 +1000

    Updated aoc.py to save and reuse puzzle data

--- puzzles/aoc.py
+++ puzzles/aoc.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DATA_DIRECTORY = 'puzzles/data/'
 class AOC():
 
     def __init__(self, puzzle_day: int):
@@ -21,6 +22,20 @@ class AOC():
             raise Exception(f'Request to {url} failed. Status code: {response.status_code}')
         return response.text.strip()
     
+    def load_puzzle(self):
+        with open(f'{DATA_DIRECTORY}day_{self.day}.txt', 'r') as file:
+            contents = file.read()
+        return contents
+    
+    def save_puzzle(self, response):
+        with open(f'{DATA_DIRECTORY}day_{self.day}.txt', 'w') as file:
+            file.write(response)
+
     def get_puzzle(self):
-        url = f'https://adventofcode.com/2023/day/{self.day}/input'
-        return self.get_request(url)
+        try:
+            puzzle = self.load_puzzle()
+        except FileNotFoundError:
+            url = f'https://adventofcode.com/2023/day/{self.day}/input'
+            puzzle = self.get_request(url)
+            self.save_puzzle(puzzle)
+        return puzzle
