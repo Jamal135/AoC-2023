e4d7b11984246246e47a1500666bcfdfa7427836
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-12-08 23:36:29 +1000

    Removed duplicated line

--- puzzles/day8.py
+++ puzzles/day8.py
@@ -43,7 +43,6 @@ def part_2(puzzle: str):
     node_index = {node['name']: node for node in data['nodes']}
     keys_length = len(data['keys'])
     starting_nodes = [node for node in node_index if node.endswith('A')]
-    node_index = {node['name']: node for node in data['nodes']}
     cycle_lengths = []
     for start_node in starting_nodes:
         node = start_node
