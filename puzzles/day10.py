5dc8011948c52cb3a603293d912e964d66107942
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-12-11 15:08:14 +1000

    Removed bad part 2 code

--- puzzles/day10.py
+++ puzzles/day10.py
@@ -29,53 +29,53 @@ def discover_start(data: List[List[str]], origin: List[int]) -> List[Union[List[
     else: raise Exception('How did we get here...')
 
 def construct_loop(data: List[List[str]], start: List[int], facing: str) -> List[str]:
-    positions = [start]
+    positions = [start + ['S']]
     while True:
         x, y = positions[-1][0], positions[-1][1]
         character = data[y][x]
         match character:
             case '|':
                 if facing == 'N':
-                    positions.append([x, y - 1])
+                    positions.append([x, y - 1, character])
                 elif facing == 'S':
-                    positions.append([x, y + 1])
+                    positions.append([x, y + 1, character])
                 else: raise Exception('Bad entry point...')
             case '-':
                 if facing == 'E':
-                    positions.append([x + 1, y])
+                    positions.append([x + 1, y, character])
                 elif facing == 'W':
-                    positions.append([x - 1, y])
+                    positions.append([x - 1, y, character])
                 else: raise Exception('Bad entry point...')
             case 'L':
                 if facing == 'S':
-                    positions.append([x + 1, y])
+                    positions.append([x + 1, y, character])
                     facing = 'E'
                 elif facing == 'W':
-                    positions.append([x, y - 1])
+                    positions.append([x, y - 1, character])
                     facing = 'N'
                 else: raise Exception('Bad entry point...')
             case 'J':
                 if facing == 'E':
-                    positions.append([x, y - 1])
+                    positions.append([x, y - 1, character])
                     facing = 'N'
                 elif facing == 'S':
-                    positions.append([x - 1, y])
+                    positions.append([x - 1, y, character])
                     facing = 'W'
                 else: raise Exception('Bad entry point...')
             case '7':
                 if facing == 'E':
-                    positions.append([x, y + 1])
+                    positions.append([x, y + 1, character])
                     facing = 'S'
                 elif facing == 'N':
-                    positions.append([x - 1, y])
+                    positions.append([x - 1, y, character])
                     facing = 'W'
                 else: raise Exception('Bad entry point...')
             case 'F':
                 if facing == 'N':
-                    positions.append([x + 1, y])
+                    positions.append([x + 1, y, character])
                     facing = 'E'
                 elif facing == 'W':
-                    positions.append([x, y + 1])
+                    positions.append([x, y + 1, character])
                     facing = 'S'
                 else: raise Exception('Bad entry point...')
             case '.':
@@ -93,32 +93,12 @@ def part_1(puzzle: str):
     loop_positions = construct_loop(data, start_position, facing)
     return math.ceil(len(loop_positions[:-1]) / 2)
 
-def find_interior(positions: List[List[int]], width: int, height: int) -> int:
-    interior_matrix = [[0 for _ in range(width)] for _ in range(height)]
-    x_values = []
-    for y in range(height):
-        x_values.append(sorted([pos[0] for pos in positions if pos[1] == y]))
-    for y in range(height):
-        if x_values[y] == []:
-            continue
-        max_x = x_values[y][-1] + 1
-        min_x = x_values[y][0]
-        for x in range(min_x, max_x):
-            if x in x_values[y]:
-                interior_matrix[y][x] += 3
-            if x not in x_values[y]:
-                interior_matrix[y][x] += 1
-    for line in interior_matrix:
-        print(line)
-    # Need to account for gaps between pipes somehow...
-    return sum(cell == 1 for row in interior_matrix for cell in row)
-
 def part_2(puzzle: str):
     data = [[char for char in string] for string in puzzle.split('\n')]
     origin_position = find_character(data, 'S')
     start_position, facing = discover_start(data, origin_position)
     loop_positions = construct_loop(data, start_position, facing)
-    return find_interior(loop_positions, len(data[0]), len(data))
+    return 'hmmm'
 
 if __name__ == '__main__':
     aoc = AOC(DAY)
